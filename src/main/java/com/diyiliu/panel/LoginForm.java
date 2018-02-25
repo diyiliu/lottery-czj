217e9564c3c06bb2e70f94ffb6a1d5f74067a9ba
DIYILIU <572772828@qq.com>
2018-02-25 21:31:09 +0800

    登录页面

--- src/main/java/com/diyiliu/panel/LoginForm.java
+++ src/main/java/com/diyiliu/panel/LoginForm.java
@@ -1,11 +1,18 @@
 package com.diyiliu.panel;
 
+import javax.swing.*;
+
 /**
  * Description: LoginForm
  * Author: DIYILIU
- * Update: 2018-02-25 20:26
+ * Update: 2018-02-25 21:05
  */
 public class LoginForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton btnLogin;
+
 
 
 }
