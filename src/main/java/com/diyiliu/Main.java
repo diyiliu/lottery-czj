918956faf3b16a59b8aa0eb2fe46f6f5098fe5d0
DIYILIU <572772828@qq.com>
2018-02-25 20:34:01 +0800

    Initial commit

---
+++ src/main/java/com/diyiliu/Main.java
@@ -0,0 +1,14 @@
+package com.diyiliu;
+
+/**
+ * Description: Main
+ * Author: DIYILIU
+ * Update: 2018-02-25 20:24
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/diyiliu/panel/LoginForm.java
@@ -0,0 +1,11 @@
+package com.diyiliu.panel;
+
+/**
+ * Description: LoginForm
+ * Author: DIYILIU
+ * Update: 2018-02-25 20:26
+ */
+public class LoginForm {
+
+
+}
